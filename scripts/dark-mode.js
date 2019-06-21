3a85cfe5092e6616951d867472a1d58e8a9bc188
Iiii-I-I-I <Iiii-I-I-I@users.noreply.github.com>
2019-06-21 03:59:20 -0400

    lazy load gallery images
    using IntersectionObserver; images get loaded when they are within 200px of the bottom of the viewport

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -13,12 +13,20 @@ document.getElementById('dark-mode-toggle').addEventListener('click', function (
     }
 });
 
-// IntersectionObserver
-// headerManipulation: function() {
-//     if ("IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype) {
-//         var e = $("body");
-//         new IntersectionObserver(function(t) {
-//             t[0].boundingClientRect.y < 0 ? e.addClass("header-not-at-top") : e.removeClass("header-not-at-top")
-//         }).observe($("#top-of-site-pixel-anchor")[0])
-//     }
-// }
+// <https://css-tricks.com/a-few-functional-uses-for-intersection-observer-to-know-when-an-element-is-in-view/>
+
+let observer = new IntersectionObserver(
+(entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            if (window.matchMedia('(-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi)').matches === true) {
+                entry.target.srcset = entry.target.dataset.srcset;
+            } else {
+                entry.target.src = entry.target.dataset.src;
+            }
+            observer.unobserve(entry.target);
+        }
+    });
+}, {rootMargin: "0px 0px 200px 0px"});
+
+document.querySelectorAll('.thumb').forEach(img => { observer.observe(img) });
